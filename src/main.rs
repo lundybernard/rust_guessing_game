7a5cb3ab9509f4f7446f2a669fd485b6f6c046d6
LundyBernard <lundy.bernard@gmail.com>
2018-12-31 15:20:10 -0600

    use external crate rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is: {}", secret_number);
+
     println!("please input your guess");
 
     let mut guess = String::new();
